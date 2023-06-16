0bbf957d927a8b5779e37e62ce9054a92fb36a55
Cristhian <criscorp_26hotmail.com>
2023-06-16 14:56:46 -0500

    creating cartshop

---
+++ Shopping/Shopping/Models/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Shopping.Models
+{
+
+    public class HomeViewModel
+    {
+        public ICollection<ProductsHomeViewModel> Products { get; set; }
+
+        public float Quantity { get; set; }
+    }
+
+}
